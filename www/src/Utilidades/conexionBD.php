449a2c5c0aefd70472201448b5a3f038eb912256
Juan Luis <juanlu_bf@hotmail.com>
2016-06-05 16:52:24 +0200

    prueba test

--- www/src/Utilidades/conexionBD.php
+++ www/src/Utilidades/conexionBD.php
@@ -1,14 +1,8 @@
 <?php
 	include_once "variablesBD.php";
 
-	$conexion = mysql_connect($host, $user, $password)
+	$conexion = mysqli_connect($host, $user, $password, $database)
 		or die("Error al conectar con el servidor");
 	//echo "Conectado a MySQL." . "<br/>" . "Identificador de conexión: $conexion" . "<br/>";
-
-	//Seleccion de la base de datos
-	
-	mysql_select_db($database, $conexion)
-		or die("Error al seleccionar la base de datos");
-	//echo "Base de datos ... seleccionada";
 
 ?>
