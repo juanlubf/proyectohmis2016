07316c61702d5dc0dbcec056c66fd9e9c6cde57d
Juan Luis <juanlu_bf@hotmail.com>
2016-06-18 19:39:40 +0200

    añadido calendario para el usuario

--- www/src/panelUsuario.php
+++ www/src/panelUsuario.php
@@ -7,21 +7,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Inicio</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css"/ >
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script type="text/javascript" src="//jonthornton.github.io/jquery-timepicker/jquery.timepicker.js"></script>
+    <link rel="stylesheet" type="text/css" href="//jonthornton.github.io/jquery-timepicker/jquery.timepicker.css" />
     <!--[if lt IE 9]<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body>
 <?php include_once 'scriptParking.php';
 global $suma;?>
+<script>
+    $(function() {
+        $( "#datepicker" ).datepicker();
+    });
+</script>
+<script>
+    $(function() {
+        $('#basicExample').timepicker();
+    });
+</script>
 <section class="container">
     <div class="pAdmin">
         <h1>Consulta de disponibilidad</h1>
         <form method="post" action="" enctype="multipart/form-data">
             <p>Introduzca la fecha y la hora</p>
-            <p><link rel="stylesheet" type="text/css" href="jquery.datetimepicker.css"/ >
-                <script src="jquery.js"></script>
-                <script src="jquery.datetimepicker.js"></script></p>
-            <p class="addPlaza">Plazas <input type="number">
-                <input type="submit" value="Confirmar" style="float: right;"></p>
+            <p><input id="datepicker" type="text">
+                <input id="basicExample" type="text" class="time" />
+            </p>
+            <p><input type="button" value="Buscar" style="float: right" onclick=""></p>
             <p><input type="button" name="volver" value="Volver" onclick="location.href='scriptLogout.php';"></p>
         </form>
     </div>
